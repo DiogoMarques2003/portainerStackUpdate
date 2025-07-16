cfb49d8c837f247dc6e06f7b13c207b7b588f16c
Diogo Marques <diogomarques2003@protonmail.com>
2025-07-16 19:52:49 +0100

    Add build and release workflow

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -6,12 +6,16 @@ from logs.base_logger import BaseLogger
 
 def resource_path(relative_path):
     '''Get absolute path to resource, works in dev and compiled versions.'''
-    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    if hasattr(sys, '_MEIPASS'):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
     return os.path.join(base_path, relative_path)
 
 def init_config(target_path):
     '''Initialize the configuration file by copying the example configuration.'''
-    example_path = resource_path(os.path.join('..', 'resources', 'example-config.yml'))
+    example_path = resource_path(os.path.join('resources', 'example-config.yml'))
 
     if os.path.exists(target_path):
         print(f'The file "{target_path}" already exists. Please remove it before generating a new configuration.')
@@ -23,7 +27,7 @@ def init_config(target_path):
     
     # Ensure the target directory exists
     target_dir = os.path.dirname(target_path)
-    if not os.path.exists(target_dir):
+    if target_dir and not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
     # Copy the example configuration file to the target path    
